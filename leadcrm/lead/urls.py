5237c6467f3d0a9455b6df07945d892d5efbf22d
Mohshaikh23 <mohsin.shaikh324@gmail.com>
2024-06-28 19:08:14 +0530

    leads editting added

--- leadcrm/lead/urls.py
+++ leadcrm/lead/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.leads_list, name='leads_list'),
+    path('<int:pk>/',views.leads_detail, name='leads_detail'),
+    path('<int:pk>/delete/',views.leads_delete, name='leads_delete'),
+    path('<int:pk>/edit/',views.edit_leads, name='edit_leads'),
     path('add-lead/', views.add_lead, name='add_lead'),
 ]
--- leadcrm/lead/views.py
+++ leadcrm/lead/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import AddLeadForm
 from .models import Lead
@@ -13,6 +14,42 @@ def leads_list(request):
                   {'leads':leads}
                   )
 
+@login_required
+def leads_detail(request, pk):
+    lead = get_object_or_404(Lead, created_by = request.user, pk=pk)
+    # lead = Lead.objects.get(pk=pk)
+
+    return render(request, 
+                'leads/leads_detail.html',
+                {'lead':lead}
+                )
+
+@login_required
+def leads_delete(request, pk):
+    lead = get_object_or_404(Lead, created_by = request.user, pk=pk)
+    lead.delete()
+    messages.success(request, "The lead was Deleted successfully.")
+
+    return render('leads_list')
+
+@login_required
+def edit_leads(request, pk):
+    lead = get_object_or_404(Lead, created_by = request.user, pk=pk)
+
+    if request.method == "POST":
+        form = AddLeadForm(request.POST, instance=lead)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "The lead was editted successfully.")
+            return redirect('leads_list')
+    else:
+        form = AddLeadForm(instance=lead)
+
+    return render(request,
+                  "leads/edit_lead.html",
+                  {'form':form}
+                  )
 @login_required
 def add_lead(request):
     if request.method == "POST":
